5490f3cbc7ea6de3e725f2f0d9088b226e38c78e
MINSU <co1359@naver.com>
2025-04-23 23:24:34 +0900

    AboutMeContent 수정

--- src/components/homepage/AboutMeContent.tsx
+++ src/components/homepage/AboutMeContent.tsx
@@ -3,8 +3,25 @@ import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import '../../css/HomePage.css';
 import MyProfile from '../../images/MyProfile.jpeg';
+import FreeMyPhoto from '../../images/FreeMyPhoto.jpeg';
 import GitHubIcon from '../../images/SkillImages/github.png';
 import TistoryIcon from '../../images/SkillImages/tistory.png';
+
+const Contain: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }}
+      animate={inView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      className="about-contact-phone" // 기존 스타일 그대로 유지
+    >
+      {children}
+    </motion.div>
+  );
+};
 
 const Box: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
@@ -38,12 +55,13 @@ const AboutMeContent: React.FC = () => {
         <h2>ABOUT ME</h2>
         <Box>
           <div className="about-image">
-            <img src={MyProfile} alt="My Photo" style={{ width: '100%', height: 'auto', transform: 'translateY(-15%)' }} />
+            <img src={FreeMyPhoto} alt="My Photo" style={{ width: '100%', height: 'auto', display: 'block', margin: '0 auto'}} />
           </div>
 
           <div className="about-contact-introduction">
           <div className="about-text">
-            <p>
+          <h3 className="about-contact-title">INTRODUCTION</h3>
+            <p className="about-contact-text">
               백석대학교 첨단IT학부를 전공하며 컴퓨터 공학 이론과 프로그래밍 및 인공지능, 빅데이터 등 다양한 분야를 학습하였습니다.
             </p>
           </div>
